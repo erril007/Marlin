8f9442458b8458dc7cce8f952a0f7b754107c11c
erril007 <141030793+erril007@users.noreply.github.com>
2023-12-25 22:55:44 +0100

    update config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1813,8 +1813,8 @@
 #define X_MIN_POS -20
 #define Y_MIN_POS 0
 #define Z_MIN_POS 0
-#define X_MAX_POS X_BED_SIZE
-#define Y_MAX_POS 365 //Y_BED_SIZE
+#define X_MAX_POS 350   //X_BED_SIZE  //FG
+#define Y_MAX_POS 365   //Y_BED_SIZE  //FG
 #define Z_MAX_POS 400
 //#define I_MIN_POS 0
 //#define I_MAX_POS 50

12c6d1adace7f67232124caac777e3bfb020cea7
erril007 <141030793+erril007@users.noreply.github.com>
2025-02-12 08:36:38 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -378,7 +378,7 @@
 // For the other hotends it is their distance from the extruder 0 hotend.
 //#define HOTEND_OFFSET_X { 0.0, 20.00 } // (mm) relative X-offset for each nozzle
 //#define HOTEND_OFFSET_Y { 0.0, 5.00 }  // (mm) relative Y-offset for each nozzle
-#define HOTEND_OFFSET_Z { 0.00, 0.00 }  // (mm) relative Z-offset for each nozzle
+#define HOTEND_OFFSET_Z { 0.00, 0.00, 0.00 }  // (mm) relative Z-offset for each nozzle
 
 // @section multi-material
 

40f970fd54e1ed784f861f76cd25e5f015a2d72d
erril007 <141030793+erril007@users.noreply.github.com>
2025-03-28 07:36:09 +0100

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1097,7 +1097,7 @@
  * Commands to execute at the end of G34 probing.
  * Useful to retract or move the Z probe out of the way.
  */
-#define EVENT_GCODE_AFTER_G34 "G60 S0\nG0 X315 Y300 F6000\nG0 X315 Y365 F6000\nG4\nM280 P0 S0\nG4\nG0 Y300 F6000\nG61 S0"
+//#define EVENT_GCODE_AFTER_G34 "G60 S0\nG0 X315 Y300 F6000\nG0 X315 Y365 F6000\nG4\nM280 P0 S0\nG4\nG0 Y300 F6000\nG61 S0"
 #endif
 
 /**

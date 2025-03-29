6589e1052da287ad1466655ec01b2ac2ac671264
erril007 <141030793+erril007@users.noreply.github.com>
2025-03-29 17:12:52 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2237,7 +2237,7 @@
 
   #define MESH_INSET 0              // Set Mesh bounds as an inset region of the bed
   #define GRID_MAX_POINTS_X 11      // Don't use more than 15 points per axis, implementation limited.
-  #define GRID_MAX_POINTS_Y 10 //GRID_MAX_POINTS_X
+  #define GRID_MAX_POINTS_Y GRID_MAX_POINTS_X
 
   #define UBL_HILBERT_CURVE       // Use Hilbert distribution for less travel when probing multiple points
 

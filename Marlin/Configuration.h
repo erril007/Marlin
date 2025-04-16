6dcaa6632e6dc4aa35d5d6e6bfe6ccf3bd5efdf9
erril007 <141030793+erril007@users.noreply.github.com>
2025-04-16 10:06:01 +0200

    update config

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -2130,7 +2130,7 @@
  * Commands to execute at the start of G29 probing,
  * after switching to the PROBING_TOOL.
  */
-#define EVENT_GCODE_BEFORE_G29 "T0\nG60 S0\nG0 X315 Y300 F6000\nG0 X315 Y365 F6000\nG4\nM280 P0 S60\nG4\nG0 Y300 F6000\nG61 S0"
+#define EVENT_GCODE_BEFORE_G29 "T0\nG60 S0\nG0 X315 F10000\nG0 Y300 F10000\nG0 Y365 F5000\nG4\nM280 P0 S60\nG4\nG0 Y300 F6000\nM851 Z0\nG28 Z"
 
 /**
  * Commands to execute at the end of G29 probing.
--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -1085,13 +1085,13 @@
   #define RESTORE_LEVELING_AFTER_G34       // Restore leveling after G34 is done?
   // After G34, re-home Z (G28 Z) or just calculate it from the last probe heights?
   // Re-homing might be more precise in reproducing the actual 'G28 Z' homing height, especially on an uneven bed.
-  #define HOME_AFTER_G34
+  //#define HOME_AFTER_G34
 
 /**
  * Commands to execute at the start of G34 probing,
  * after switching to the PROBING_TOOL.
  */
-#define EVENT_GCODE_BEFORE_G34 "T0\nG0 X315 Y300 F6000\nG0 X315 Y365 F6000\nG4\nM280 P0 S60\nG4\nG0 Y300 F6000"
+#define EVENT_GCODE_BEFORE_G34 "T0\nG60 S0\nG0 X315 F10000\nG0 Y300 F10000\nG0 Y365 F5000\nG4\nM280 P0 S60\nG4\nG0 Y300 F5000\nM851 Z0\nG28 Z"
 
 /**
  * Commands to execute at the end of G34 probing.

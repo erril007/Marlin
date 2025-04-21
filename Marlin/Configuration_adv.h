76af9cf2a0652d4c4a7234148343031d49fa1e63
erril007 <141030793+erril007@users.noreply.github.com>
2025-04-21 13:57:35 +0200

    Update Configuration_adv.h

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2376,7 +2376,7 @@
      * Higher k and higher XY acceleration may require larger ADVANCE_TAU to avoid skipping steps.
      */
     #if ENABLED(DISTINCT_E_FACTORS)
-      #define ADVANCE_TAU { 0.01 }   // (s) Smoothing time to reduce extruder acceleration, per extruder
+      #define ADVANCE_TAU { 0.01, 0.01 }   // (s) Smoothing time to reduce extruder acceleration, per extruder
     #else
       #define ADVANCE_TAU 0.01       // (s) Smoothing time to reduce extruder acceleration
     #endif

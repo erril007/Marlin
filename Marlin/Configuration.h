ad46b243fc7d823c8fd9826d61dec9d4c239c92a
erril007 <141030793+erril007@users.noreply.github.com>
2026-01-25 19:54:14 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1363,7 +1363,7 @@
  * Override with M201
  *                                      X, Y, Z [, I [, J [, K...]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_ACCELERATION      { 10000, 10000, 500, 8000, 8000 }
+#define DEFAULT_MAX_ACCELERATION      { 10000, 10000, 100, 8000, 8000 }
 
 //#define LIMITED_MAX_ACCEL_EDITING     // Limit edit via M201 or LCD to DEFAULT_MAX_ACCELERATION * 2
 #if ENABLED(LIMITED_MAX_ACCEL_EDITING)
